32f5294e2ea0c5e656d614c62050bd958ddc9619
Safal Gupta <safalgupta666534@gmail.com>
2026-09-21 01:07:21 +0530

    changed auth

--- OnlineExam-UI/src/context/AuthContext.tsx
+++ OnlineExam-UI/src/context/AuthContext.tsx
@@ -3,7 +3,7 @@ import {
   createContext, //creates global storage/access system
   useEffect,
   useState,
-  type ReactNode,
+  type ReactNode, //tell typescript what children can contain
 } from "react";
 
 import type { User } from "../types/user";
@@ -14,28 +14,32 @@ interface AuthContextType {
   logout: () => void;
 }
 
-//it creates actual context global box/storage
-export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined 
+//it creates actual context global box/storage so it can pass who is logged in directly to component like(dashboard,navbar,profile etc.)
+export const AuthContext = createContext<AuthContextType | undefined>(  ////explain auth context in simple words line by line still didnt get what and why
+  undefined   //initially undefined
 );
 
+
+//"I want to create a component called AuthProvider, and it can contain other React components inside it."
+//
 interface AuthProviderProps {
   children: ReactNode;
 }
 
-//provided context to authcontext
+//provide context to authcontext Keep track of the logged-in user and give that information to everything inside it.
 export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<User | null>(() => {
-    const savedUser = localStorage.getItem("onlineExamUser");
+  const [user, setUser] = useState<User | null>(() => {// remember this current user for me
+    const savedUser = localStorage.getItem("onlineExamUser"); //"When my app starts, check whether we previously saved a logged-in user."
 
     return savedUser ? JSON.parse(savedUser) : null;
   });
 
   useEffect(() => {
-    if (user) {
-      localStorage.setItem("onlineExamUser", JSON.stringify(user));
+    if (user) { //if someone loged in
+      //Browser has something called localStorage. think of it as small storage box
+      localStorage.setItem("onlineExamUser", JSON.stringify(user)); //save user info in browser
     } else {
-      localStorage.removeItem("onlineExamUser");
+      localStorage.removeItem("onlineExamUser"); //if no one loged remove user
     }
   }, [user]);
 
@@ -47,8 +51,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
     setUser(null);
   }
 
+  ////means give these 3 things to everyone inside it
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, logout }}> 
       {children}
     </AuthContext.Provider>
   );
